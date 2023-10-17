6b80f13258a4e387001c46da888e256b33808a5a
abhishek.khapariye <abhishek.khapariye@ksolves.com>
2023-10-17 18:54:20 +0530

    Practice basics of js

---
+++ script.js
@@ -0,0 +1,4 @@
+// alert("Wellcome")
+// document.write("Wellcome Abhishek")
+console.log("Hii java")
+console.log("hbfhk")
---
+++ var_let_const.js
@@ -0,0 +1,20 @@
+console.log("javascript with var_let_const");
+var a = 45;
+var b = "abhi";
+var c = null;
+var d = undefined;
+let e = 66;
+const z = 33;
+
+
+   
+    e = 77; // let e=46; exception let can be updated not redeclaired
+    a = 78;// var can be updated and redeclaired also within its scope
+    // const z = 56; //cannot updated nor redeclaired
+
+console.log(a);
+console.log(b);
+console.log(e);
+console.log(z);
+console.log(d);
+console.log(c);
